9a7e5a04769371a9136b61860abe532b021c6f62
Gareth Dunstone <freedom.2.the.leetle.people@gmail.com>
2018-03-19 13:05:08 +1100

    added gitignore, added git alert mechanism.

---
+++ gitalert.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import sys
+import json
+from github import Github, GithubObject
+config = json.load(open('/home/gareth/tickscripts/config.json'))
+g = Github(config['token'])
+
+user = g.get_organization(config['org'])
+repo = user.get_repo(config['repo'])
+data = json.load(sys.stdin)
+
+alert_title = "{}-{}".format(data['id'], sys.argv[1])
+full_title = "[{}] {}".format(data['level'], alert_title)
+
+def make_issue():
+    kwargs = {
+        "body": data['message'],
+        "labels": [data['level']]
+    }
+    if "," in sys.argv[2]:
+        kwargs["assignees"] = sys.argv[2].split(',')
+    else:
+        kwargs["assignee"] = sys.argv[2].strip()
+
+    repo.create_issue(full_title, **kwargs)
+
+for iss in repo.get_issues():
+    if iss.title.contains(alert_title):
+        iss.create_comment()
+        sys.exit(0)
+
+make_issue()
