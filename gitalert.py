7ace84532628e1357488285be5bf4a000906b8d0
Gareth Dunstone <freedom.2.the.leetle.people@gmail.com>
2018-08-27 14:39:40 +1000

    added slack updates for issues.

--- gitalert.py
+++ gitalert.py
@@ -5,12 +5,14 @@ import requests
 import datetime
 import yaml
 from github import Github, GithubObject
-config = json.load(open('/home/gareth/tickscripts/config.json'))
+config = json.load(open('/home/stormaes/tickscripts/config.json'))
 g = Github(config['token'])
 
 user = g.get_organization(config['org'])
 repo = user.get_repo(config['repo'])
 data = json.load(sys.stdin)
+
+slack_hook = config.get("slack_webhook")
 
 full_title = "[{}] {}".format(data['level'], data['id'])
 
@@ -31,6 +33,32 @@ all_assignees = schedule_data['notify_on_days'][day_of_week]
 if always is not None and always not in ["", ",", " "]:
     all_assignees = ",".join([always, schedule_data['notify_on_days'][day_of_week]])
 
+def notify_slack(issue, updated=False):
+
+    color = "#36a64f"
+    if "CRITICAL" in data['level']:
+        color = "#FF0000"
+    request_data = {
+        "attachments": [
+            {
+                "fallback": data['message'],
+                "color": color,
+                "pretext": data['id'],
+                "title": issue.title,
+                "title_link": issue.html_url,
+                "text": "This git issue was just {}".format("created" if not updated else "updated"),
+                "footer": "Kapacitor"
+            }
+        ]
+    }
+
+    r = requests.post(
+        slack_hook,
+        data = json.dumps(request_data),
+        headers = {'Content-Type': 'application/json'}
+    )
+    print(r)
+
 def make_issue():
     kwargs = {
         "body": data['message'],
@@ -42,11 +70,13 @@ def make_issue():
     else:
         kwargs["assignee"] = all_assignees.strip()
 
-    repo.create_issue(full_title, **kwargs)
+    return repo.create_issue(full_title, **kwargs)
 
 for iss in repo.get_issues():
     if data['id'] in iss.title:
         iss.create_comment(data['message'])
+        notify_slack(iss, data['message'])
         sys.exit(0)
 
-make_issue()
+iss = make_issue()
+notify_slack(iss)
