9f0af9edf0b6619ac5b6e2269ecaacc71730cbb3
Gareth Dunstone <freedom.2.the.leetle.people@gmail.com>
2019-03-21 13:30:11 +1100

    adding data export

---
+++ python/writecsv.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import sys, os
+import json
+import requests
+import datetime
+import yaml
+import time
+import re
+slack_hook = config.get("slack_webhook")
+curpath = os.path.abspath(__file__)
+mydir = os.path.dirname(curpath)
+config = json.load(open(os.path.join(mydir, 'config.json')))
+
+data = json.load(sys.stdin)
+
+slack_hook = config.get("slack_webhook")
+with open("/etc/data/test-{}.json".format(datetime.datetime.now().isoformat()), 'w') as f:
+    json.dumps(data, f, indent=4)
