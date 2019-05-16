6adcef3af305ae8405336b1be9f9f9b225bb0b60
Gareth Dunstone <freedom.2.the.leetle.people@gmail.com>
2019-05-16 13:19:15 +1000

    updates, currently running

--- python/writecsv.py
+++ python/writecsv.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sys, os
 import json
+import gzip
 import requests
 import datetime
 import yaml
@@ -11,11 +12,15 @@ curpath = os.path.abspath(__file__)
 mydir = os.path.dirname(curpath)
 config = json.load(open(os.path.join(mydir, 'config.json')))
 
-slack_hook = config.get("slack_webhook")
+# slack_hook = config.get("slack_webhook")
 data = json.load(sys.stdin)
+os.makedirs(os.path.join("/data/", sys.argv[1]), exist_ok=True)
+
 for series in data['data']['series']:
     header = ",".join(series['columns'])
-    with open("/data/{}_{}.csv".format((datetime.datetime.now()-datetime.timedelta(hours=24)).strftime("%Y-%m-%d"),series['name']), 'w') as f:
+    pathname = "{}_{}.csv.gz".format((datetime.datetime.now()-datetime.timedelta(hours=24)).strftime("%Y-%m-%d"),series['name'])
+    with gzip.open(os.path.join("/data/", sys.argv[1], pathname), 'wt') as f:
         f.write(header+"\n")
         for tp in series['values']:
             f.write(",".join([str(x) for x in tp])+"\n")
+
